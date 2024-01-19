8f8bd15586c308521541ac06fa8a574f128c8dba
kayden <it@otherhome.org>
2024-01-19 17:28:47 +1030

    Done multiple points for stores

--- theme/BS4-Basic/storage/widget/widget-rank-mb-rank-1-pc.php
+++ theme/BS4-Basic/storage/widget/widget-rank-mb-rank-1-pc.php
@@ -1,7 +1,7 @@
 <?php
 if (!defined('_GNUBOARD_')) exit;
 $data=array (
-  'mb_list' => 'AdelaideInside,kayden,popul9',
+  'mb_list' => 'chunpung2,iamjay,adelaideinside,Kayden,jjhtesy,popul9,iamjay3,yjleejimmy,otherhome,IamJay2,donalmum525',
   'cache' => '',
   'rank' => '',
   'mode' => 'post',
