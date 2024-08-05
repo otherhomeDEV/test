f13cb87efe3fe3746b54c649da06c1c458324377
kayden <it@otherhome.org>
2024-08-05 12:39:55 +0930

    edited layout regarding hit rate

--- theme/BS4-Basic/side/side-index.php
+++ theme/BS4-Basic/side/side-index.php
@@ -54,7 +54,7 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 					</h3>
 					<hr class="hr"/>
 					<a href="<?php echo G5_BBS_URL ?>/board.php?bo_table=todayinside">
-					<div class="px-3 px-sm-0 my-3">
+					<div class="px-3 px-sm-0 my-3" style="text-align: center;">
 						<img src="https://adelaideinside.com/data/editor/2408/thumb-606b454b6b4a625b8eb3816537c067ab_1722823162_545_400x225.png" alt="투데이인사이드썸네일" class="na-round" title="" style="width:245px;, height: 138px;">
 					</div>
 					</a>
