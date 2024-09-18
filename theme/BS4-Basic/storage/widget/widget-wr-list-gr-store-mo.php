1b099dc9d26581e82e98ebbe6deace6e9d558126
kayden <it@otherhome.org>
2024-09-18 15:44:58 +0930

    trimed boards

---
+++ theme/BS4-Basic/storage/widget/widget-wr-list-gr-store-mo.php
@@ -0,0 +1,19 @@
+<?php
+if (!defined('_GNUBOARD_')) exit;
+$data=array (
+  'cache' => '5',
+  'bo_new' => '',
+  'rank' => '',
+  'target' => '',
+  'icon' => '',
+  'bo_name' => '',
+  'gr_list' => '',
+  'bo_list' => 'store',
+  'ca_list' => '',
+  'mb_list' => '',
+  'rows' => '4',
+  'page' => '',
+  'sort' => '',
+  'term' => '',
+  'dayterm' => '',
+);
