309865af66c891224b1f896898b90d04a8eda53a
kayden <kayden190116@gmail.com>
2024-10-02 15:03:37 +0930

    fixed side banner

--- theme/BS4-Basic/side/side-index.php
+++ theme/BS4-Basic/side/side-index.php
@@ -9,40 +9,6 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 
                  
 				
-
-	               <!-- 위젯 시작 { -->
-					 <!--<h3 class="h3 f-lg en" style="margin-top:30px;" >
-						<a href="<?php echo G5_BBS_URL ?>/board.php?bo_table=event">
-							업체게시판 및 안내
-						</a>
-					</h3>
-					<hr class="hr"/>
-					<div class="mt-3 mb-4">
-						<?php echo na_widget('wr-list', 'tlist-1', 'bo_table=event'); ?>
-					</div>
-					<!-- } 위젯 끝-->
-
-                <!-- 위젯 시작 { -->
-					 <!--<h3 class="h3 f-lg en" style="margin-top:42px;">
-						<a href="<?php echo G5_BBS_URL ?>/board.php?bo_table=findjob">
-							일자리
-						</a>
-					</h3>
-					<hr class="hr"/>
-					<div class="mt-3 mb-4">
-						<?php echo na_widget('wr-list', 'tlist-2', 'gr_id=jobs'); ?>
-					</div>
-					<!-- } 위젯 끝-->
-                    위젯 시작 { -->
-					 <!--<h3 class="h3 f-lg en" style="margin-top:42px;"> <!--  style="margin-bottom:40px;" 위젯 레이아웃 아래마진 - kayden { -->
-						 <!--<a href="<?php echo G5_BBS_URL ?>/board.php?bo_table=share">
-							렌트/쉐어
-						</a>
-					</h3>
-					<hr class="hr"/>
-					<div class="mt-3 mb-4">
-						<?php echo na_widget('wr-list', 'tlist-3', 'gr_id=property'); ?>
-					</div>-->
 					
 					<!-- 위젯 시작 { -->
 						<h3 class="h3 f-lg en">
@@ -63,23 +29,9 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 						</div>
 					<!-- } 위젯 끝-->
 
-					<!-- } 위젯 끝-->
-					 <!--	
-					<!-- 위젯 시작 { -->
-					<!--<h3 class="h3 f-lg en">
-					<a href="<?php echo G5_BBS_URL ?>/board.php?bo_table=todayinside">
-						투데이 인사이드 보러가기
-					</a>
-					</h3>
-					<hr class="hr"/>
-					<a href="<?php echo G5_BBS_URL ?>/board.php?bo_table=todayinside">
-					<div class="px-3 px-sm-0 my-3" style="text-align: center;">
-						<img src="https://adelaideinside.com/data/editor/2408/thumb-606b454b6b4a625b8eb3816537c067ab_1722823162_545_400x225.png" alt="투데이인사이드썸네일" class="na-round" title="" style="width:245px;, height: 138px;">
-					</div>-->
-					</a>
-					<!-- } 위젯 끝-->
+				
+		
 
-					<!-- 위젯 시작 { -->
 					<!--<h3 class="h3 f-lg en">
 					<a href="<?php echo G5_BBS_URL ?>/board.php?bo_table=notice&wr_id=437">
 						웹사이트 만들어 드립니다
